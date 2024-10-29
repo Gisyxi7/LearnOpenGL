9bd7b640a18be80af6b4a1a1fed8bf6b7d3bf40c
Yuxuan Guo <fengliyousuanyoutian@gmail.com>
2024-10-29 21:32:28 +0800

    hello window

--- main.cpp
+++ main.cpp
@@ -3,7 +3,68 @@
 
 #include <iostream>
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
 int main(){
+    /*设置主版本号(Major)和次版本号(Minor)
+    告诉GLFW我们使用的是核心模式(Core-profile)*/
+    //------------------------
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    /*创建窗口
+    通知GLFW将我们窗口的上下文设置为当前线程的主上下文*/
+    //------------------------
+    GLFWwindow* window= glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT,"LearnOpenGL",NULL,NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    /*在调用任何OpenGL的函数之前我们需要初始化GLAD*/
+    //------------------------
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    /*主循环*/
+    //------------------------
+    while(!glfwWindowShouldClose(window))
+    {
+        //输入
+        processInput(window);
+
+        //渲染指令
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        //检查并调用事件，交换缓冲
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    /*正确释放/删除之前的分配的所有资源*/
+    //------------------------
+    glfwTerminate();
 
     return 0;
 }
